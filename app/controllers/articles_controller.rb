c2db9c8b78afbe407c1cf8c9518981c9ba837d05
adj138 <adj138@gmail.com>
2016-10-10 04:33:12 +0000

    removed final redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :destroy]
     
     def index
       @allarticles = Article.all
@@ -19,11 +20,11 @@ class ArticlesController < ApplicationController
     end
     
     def show
-      @article = Article.find(params[:id])
+      
     end
     
     def edit
-       @article = Article.find(params[:id])
+       
     end
 
     def update
@@ -37,7 +38,7 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-      @article = Article.find(params[:id])
+      
       @article.destroy
       
       flash[:notice] = 'Article was succesfully deleted'
@@ -45,6 +46,11 @@ class ArticlesController < ApplicationController
     end
     
     private
+      
+      def  set_article
+        @article = Article.find(params[:id])
+      end
+      
       def article_params
         params.require(:article).permit(:title, :description)
       end
